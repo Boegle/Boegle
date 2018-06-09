9dc31c4c902d958c0abd0f3f7d723175038a96f1
Victor Zumpolle <victor@victorzumpolle.nl>
2018-06-09 13:18:36 +0200

    ADD - All selectors to send all data through sockets

--- src/js/modules/formResults.js
+++ src/js/modules/formResults.js
@@ -4,18 +4,32 @@ let bookForm = {
   init: function() {
     console.log('bookForm init complete')
     document.querySelector('form').addEventListener('submit', this.search)
+    bookForm.select.inputs.forEach(input => input.addEventListener('change', bookForm.search))
+    bookForm.select.selects.forEach(select => select.addEventListener('change', bookForm.search))
     socket.init()
+  },
+  select: {
+    inputs: document.querySelectorAll('#bookInfo input'),
+    selects: document.querySelectorAll('#bookInfo select'),
+    checkboxes: document.querySelectorAll('#checkboxes input'),
+    title: document.querySelector('#title'),
+    author: document.querySelector('#author'),
+    language: document.querySelector('#language'),
+    age: document.querySelector('#age'),
+    year: document.querySelector('#year'),
+    illustrator: document.querySelector('#illustrator'),
+    illustrations: document.querySelector('#illustrations'),
+    publisher: document.querySelector('#publisher'),
+    pages: document.querySelector('#pages'),
+    summary: document.querySelector('#summary'),
+    coverColor: document.querySelector('#coverColor'),
+    coverSummary: document.querySelector('#coverSummary')
   },
   search: function(el) {
     el.preventDefault()
-    const input = document.querySelectorAll('#bookInfo input')
-    const select = document.querySelectorAll('#bookInfo select')
-    const checkboxes = document.querySelectorAll('#checkboxes input')
     const genres = []
 
-    console.log('test')
-
-    checkboxes.forEach((checkbox) => {
+    bookForm.select.checkboxes.forEach((checkbox) => {
       if (checkbox.checked) {
         genres.push(checkbox.value)
       }
@@ -23,12 +37,19 @@ let bookForm = {
 
     const givenSearchValues = {
       url: 'search',
-      title: input[0].value,
-      author: input[1].value,
-      language: select[0].value,
-      age: select[1].value,
-      pubYear: input[2].value,
-      genres: genres
+      title: bookForm.select.title.value,
+      author: bookForm.select.author.value,
+      language: bookForm.select.language.value,
+      age: bookForm.select.age.value,
+      pubYear: bookForm.select.year.value,
+      genres: genres,
+      illustrator: bookForm.select.illustrator.value,
+      illustrations: bookForm.select.illustrations.value,
+      publisher: bookForm.select.publisher.value,
+      pages: bookForm.select.pages.value,
+      summary: bookForm.select.summary.value,
+      coverColor: bookForm.select.coverColor.value,
+      coverSummary: bookForm.select.coverSummary.value
     }
     socket.io.emit('searchValues', givenSearchValues)
   }
