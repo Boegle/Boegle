6a7c08f94cb6bab8ec52f80c5e4810778c89645d
muise001 <emiel.muis@hva.nl>
2018-06-11 16:57:59 +0200

    FIX - Book animation made smoother

--- src/js/modules/book.js
+++ src/js/modules/book.js
@@ -1,11 +1,33 @@
 const mainBook = {
   init: function() {
-    console.log('tekfposjgpodsjposdjgpodjst')
-    document.querySelector('#book .cover').addEventListener('click', this.flipPage)
+    console.log('appelsap')
+    document.querySelectorAll('#buttons a').forEach((button) => {
+      button.addEventListener('click', this.flipPage)
+    })
   },
+  flipCount: 0,
   flipPage: function() {
-    this.classList.add('animation')
-    document.querySelectorAll('.page')[1].classList.add('animation')
+    console.log(mainBook.flipCount)
+    if (this.id == 'next') {
+      mainBook.flipCount ++
+    } else {
+      mainBook.flipCount --
+    }
+    mainBook.actualFlipPage()
+  },
+  actualFlipPage: function() {
+    if (this.flipCount == 1) {
+      document.querySelectorAll('.page')[0].classList.add('animation')
+      document.querySelectorAll('.page')[1].classList.add('animation')
+    } else if (this.flipCount == 2) {
+      document.querySelectorAll('.page')[2].classList.add('animation')
+      document.querySelectorAll('.page')[3].classList.add('animation')
+    } else if (this.flipCount == 3) {
+      document.querySelectorAll('#book .page').forEach((page) => {
+        // page.classList.remove('animation')
+        page.classList.add('animationReverse')
+      })
+    }
   }
 }
 
