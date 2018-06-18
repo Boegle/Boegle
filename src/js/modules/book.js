14476523700ab41c4e642eaa30a425298afe1a35
muise001 <emiel.muis@hva.nl>
2018-06-18 12:35:54 +0200

    Back button works

--- src/js/modules/book.js
+++ src/js/modules/book.js
@@ -10,37 +10,70 @@ const mainBook = {
         document.querySelectorAll('.cover')[0].style.setProperty('--cover-translateY', '-' + parseInt(value / 100) + 'em')
         document.querySelectorAll('.bookBottomTwo')[0].style.setProperty('--bookBottom-scale', 1 + parseInt(value /100))
         console.log(value)
-        document.querySelectorAll('.page').forEach((page) => {
+        this.selectors.page.forEach((page) => {
           page.classList.add('none')
         })
-        document.querySelectorAll('.page')[0].classList.remove('none')
+        this.selectors.page[0].classList.remove('none')
         document.querySelector('#pageSlideIndicator').innerHTML = value
       })
     }
   },
+  selectors : {
+    page: document.querySelectorAll('.page'),
+    listItem: document.querySelectorAll('#sideNav li')
+  },
+  currentState: 0,
   flipCount: 0,
   flipPage: function() {
-    console.log(mainBook.flipCount)
     if (this.id == 'next') {
       mainBook.flipCount ++
     } else {
       mainBook.flipCount --
     }
+    console.log('van' + mainBook.currentState)
+    console.log('naar' + mainBook.flipCount)
     mainBook.actualFlipPage()
   },
   actualFlipPage: function() {
-    if (this.flipCount == 1) {
-      document.querySelectorAll('.page')[0].classList.add('animation')
-      document.querySelectorAll('.page')[1].classList.add('animation')
+    if (this.currentState == 0 && this.flipCount == 1) {
+      this.selectors.page[0].classList.add('animation')
+      this.selectors.page[1].classList.add('animation')
       document.querySelectorAll('#sideNav li')[0].classList.remove('checked')
       document.querySelectorAll('#sideNav li')[1].classList.add('checked')
-    } else if (this.flipCount == 2) {
-      document.querySelectorAll('.page')[2].classList.add('animation')
-      document.querySelectorAll('.page')[3].classList.add('animation')
+      this.currentState++
+    } else if (this.currentState == 1 && this.flipCount == 0) {
+      this.selectors.page[1].classList.add('animationReverse')
+      this.selectors.page[0].classList.add('animationReverse')
+      this.selectors.page[0].classList.remove('animation')
+      this.selectors.page[1].classList.remove('animation')
+      setTimeout (() => {
+        this.selectors.page[1].classList.remove('animationReverse')
+        this.selectors.page[0].classList.remove('animationReverse')
+      }, 1500)
+      document.querySelectorAll('#sideNav li')[0].classList.add('checked')
+      document.querySelectorAll('#sideNav li')[1].classList.remove('checked')
+      this.currentState--
+    } else if (this.currentState == 1 && this.flipCount == 2) {
+      this.selectors.page[2].classList.add('animation')
+      this.selectors.page[3].classList.add('animation')
       document.querySelectorAll('#sideNav li')[1].classList.remove('checked')
       document.querySelectorAll('#sideNav li')[2].classList.add('checked')
-    } else if (this.flipCount == 3) {
+      this.currentState++
+    } else if (this.currentState == 2 && this.flipCount == 1) {
+      this.selectors.page[3].classList.add('animationReverse')
+      this.selectors.page[2].classList.add('animationReverse')
+      this.selectors.page[2].classList.remove('animation')
+      this.selectors.page[3].classList.remove('animation')
+      setTimeout (() => {
+        this.selectors.page[2].classList.remove('animationReverse')
+        this.selectors.page[3].classList.remove('animationReverse')
+      }, 1500)
+      document.querySelectorAll('#sideNav li')[1].classList.add('checked')
       document.querySelectorAll('#sideNav li')[2].classList.remove('checked')
+      this.currentState--
+    } else if (this.currentState == 2 && this.flipCount == 3) {
+      document.querySelectorAll('#sideNav li')[2].classList.remove('checked')
+      this.currentState++
       document.querySelectorAll('#book .page').forEach((page) => {
         page.classList.add('animationReverse')
         setTimeout(() => {
@@ -48,10 +81,19 @@ const mainBook = {
           document.querySelectorAll('#sideNav li')[3].classList.add('checked')
           document.querySelector('.bookBottomTwo').style.setProperty('--bookBottom-scale', 1)
           document.querySelector('#book').classList.add('inDepth')
-
           console.log('sapje')
         }, 500)
       })
+    } else if (this.currentState == 3 && this.flipCount == 2) {
+      document.querySelectorAll('#book .page').forEach((page) => {
+        page.classList.remove('animationReverse')
+        page.classList.add('animation')
+      })
+      this.currentState--
+      document.querySelector('#range').classList.remove('sliderSlidesIn')
+      document.querySelectorAll('#sideNav li')[3].classList.remove('checked')
+      document.querySelector('.bookBottomTwo').style.setProperty('--bookBottom-scale', 0)
+      document.querySelector('#book').classList.remove('inDepth')
     }
   },
   sideMenu: function() {
