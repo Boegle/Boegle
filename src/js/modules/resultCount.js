9e781b5fb7c0411d8ebf1f8282f4e9cb1016844e
Victor Zumpolle <victor@victorzumpolle.nl>
2018-06-18 14:14:44 +0200

    ADD - Removing and adding classes

--- src/js/modules/resultCount.js
+++ src/js/modules/resultCount.js
@@ -3,20 +3,23 @@ const result = {
     const resultButton = document.querySelector('#resultButton')
 
     if(data < 401 && data > 40) {
-      resultButton.disabled = false
+      resultButton.classList.remove('inactive')
+      resultButton.classList.remove('noresult')
       resultButton.innerHTML = 'Toon resultaten'  
     } else if(data < 41 && data > 1) {
-      resultButton.disabled = false
+      resultButton.classList.remove('inactive')
+      resultButton.classList.remove('noresult')
       resultButton.innerHTML = 'Toon ' + data + ' resultaten'
     } else if(data === 1) {
-      resultButton.disabled = false
+      resultButton.classList.remove('inactive')
+      resultButton.classList.remove('noresult')
       resultButton.innerHTML = 'Toon ' + data + ' resultaat'
     } else if(data === 0) {
-      resultButton.disabled = true
+      resultButton.classList.remove('inactive')
+      resultButton.classList.add('noresult')
       resultButton.innerHTML = 'Geen resultaten'
     } else {
-      resultButton.disabled = true
-      resultButton.innerHTML = 'Toon resultaten'
+      resultButton.classList.add('inactive')
     }
     console.log(data)
   }
