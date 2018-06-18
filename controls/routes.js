a5d88fdd3995eb1fdba2ac6a7da8c7638b009484
Victor Zumpolle <victor@victorzumpolle.nl>
2018-06-18 15:38:10 +0200

    ADD - Data will go through processing

--- controls/routes.js
+++ controls/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const api = require('./api')
+const processData = require('./process')
 
 router.get('/', (req, res) => {
   res.render('index')
@@ -8,10 +9,21 @@ router.get('/', (req, res) => {
 
 router.get('/result', (req, res) => {
   console.log(api.dataObj)
-  api.getResults(api.dataObj).then((data) => {
-    console.log(data)
-    res.render('result')
-  })
+  api.getResults(api.dataObj)
+    .then((data) => {    
+      let resultNumber = Number(data.aquabrowser.meta[0].count[0])
+      let pages = Math.ceil(resultNumber / 20)
+      console.log(resultNumber, pages)
+      if(pages < 6) {
+        return processData.resultPage(data)
+      } else {
+        console.log('Get results...')
+      }
+    })
+    .then((processedData) => {
+      console.log(processedData)
+      res.render('result', processedData)
+    })
 })
 
 router.get('/detail', (req, res) => {
