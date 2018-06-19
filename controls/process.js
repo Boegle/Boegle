12f46de62ae94b042afadf2557df900ad5ef8164
Victor Zumpolle <victor@victorzumpolle.nl>
2018-06-19 18:20:24 +0200

    ADD - Checks if data is included

--- controls/process.js
+++ controls/process.js
@@ -9,27 +9,23 @@ const processData = {
     return parsedData    
   },
   result: function(data, parsedData) {
+    console.log(parsedData)
     data.resultNumber = Number(parsedData.aquabrowser.meta[0].count[0])
     return data
   },
   resultPage: function(data) {    
-    console.log(data)
     let dataObj = data.aquabrowser.results[0].result.map((result) => {
       let ppn = ''
       let title = result.titles[0]['short-title'][0]._
       let titleIndex = title.indexOf(':')
+
       title = title.substring(0, titleIndex != -1 ? titleIndex : title.length)
-      console.log(result.identifiers)
+
       if(result.identifiers[0]['ppn-id']) {
         ppn = result.identifiers[0]['ppn-id'][0]._
       }
-      
       return {
         title: title,
-        author: result.authors[0]['main-author'][0]._,
-        publisher: 'lemniscaat',
-        year: '2016',
-        pages: '252',
         ppn: ppn,
         obaId: result.id[0].$.nativeid
       }
@@ -38,17 +34,68 @@ const processData = {
   },
   detailPage: function(data) {
     console.log(data.aquabrowser)
-    let dataObj = {
-      title: data.aquabrowser.titles[0]['short-title'][0]._,
-      author: data.aquabrowser.authors[0]['main-author'][0]._,
-      ppn: data.aquabrowser.identifiers[0]['ppn-id'][0]._
-    }
+    let summary = ''
+    let ppn = ''
+    let publisher = []
+    let year = 'Publicatie jaar onbekend'
+    let topic = ''
+
+    let title = data.aquabrowser.titles[0]['short-title'][0]._
+    let titleIndex = title.indexOf(':')
+    let undertitle = title.substring(titleIndex != -1 ? titleIndex + 2 : title.length, title.length)
+    title = title.substring(0, titleIndex != -1 ? titleIndex : title.length)
+
+    let pages = data.aquabrowser.description[0]['physical-description'][0]._
+    let pagesIndex = pages.indexOf(' ')
+    pages = pages.substring(0, pagesIndex != -1 ? pagesIndex : pages.length)
 
     if(data.aquabrowser.summaries) {
-      dataObj.summary = data.aquabrowser.summaries[0]['summary'][0]._
+      summary = data.aquabrowser.summaries[0]['summary'][0]._
     }
-    return dataObj
+
+    if(data.aquabrowser.identifiers[0]['ppn-id']) {
+      ppn = data.aquabrowser.identifiers[0]['ppn-id'][0]._
+    }
+
+    if(data.aquabrowser.subjects) {
+      topic = data.aquabrowser.subjects[0]['topical-subject'][0]._
+    } else {
+      topic = title
+    }
+
+    if(data.aquabrowser.publication[0].publishers[0]['publisher']) {
+      data.aquabrowser.publication[0].publishers[0]['publisher'].forEach(onePublisher => {
+        if(onePublisher._) {
+          publisher.push(onePublisher._)
+        }
+
+        if(onePublisher.$.year) {
+          year = onePublisher.$.year
+        }
+      })
+    }
+    return {
+      title: title,
+      undertitle: undertitle,
+      author: data.aquabrowser.authors[0]['main-author'][0]._,
+      publisher: publisher,
+      year: year,
+      pages: pages,
+      ppn: ppn,
+      summary: summary,
+      topic: topic,
+      obaId: data.aquabrowser.id[0].$.nativeid
+    }
   },
+  availability: function(data) {
+    let dataObj = data.aquabrowser.locations[0].location.map((location) => {
+      return {
+        location: location.$.name,
+        availability: location.$.available
+      }
+    })
+    return dataObj
+  }, 
   send: function(io, socket, data) {
     io.to(socket.id).emit('searchCount', data.resultNumber)
   } 
