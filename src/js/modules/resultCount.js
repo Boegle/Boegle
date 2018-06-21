6f147f11b3af3e6494efb404e9f50b1647c28849
Victor Zumpolle <victor@victorzumpolle.nl>
2018-06-21 15:53:22 +0200

    ADD - Working navigation with tab

--- src/js/modules/resultCount.js
+++ src/js/modules/resultCount.js
@@ -5,23 +5,28 @@ const result = {
     if(data < 401 && data > 20) {
       resultButton.classList.remove('inactive')
       resultButton.classList.remove('noresult')
-      resultButton.innerHTML = 'Toon 40 resultaten'  
+      resultButton.innerHTML = 'Toon 40 resultaten' 
+      resultButton.tabIndex = 0 
     } else if(data < 21 && data > 1) {
       resultButton.classList.remove('inactive')
       resultButton.classList.remove('noresult')
       resultButton.innerHTML = 'Toon ' + data + ' resultaten'
+      resultButton.tabIndex = 0
     } else if(data === 1) {
       resultButton.classList.remove('inactive')
       resultButton.classList.remove('noresult')
       resultButton.innerHTML = 'Toon ' + data + ' resultaat'
+      resultButton.tabIndex = 0
     } else if(data === 0) {
       resultButton.classList.remove('inactive')
       resultButton.classList.add('noresult')
       resultButton.innerHTML = 'Geen resultaten'
+      resultButton.tabIndex = -1
     } else {
       resultButton.classList.add('noresult')
       resultButton.classList.add('inactive')
       resultButton.innerHTML = data + ' resultaten'
+      resultButton.tabIndex = -1
     }
   }
 }
