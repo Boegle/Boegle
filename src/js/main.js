bec6331b47c8094bceee23cd1890af75589b1978
DesleyAalderink <desley-aalderink@hotmail.com>
2018-06-26 14:44:16 +0200

    ADD - Zero state that only works for the homepage

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import{bookForm} from './modules/formResults.js'
 import{mainBook} from './modules/book.js'
 import{result} from './modules/result.js'
 import{bookLocation} from './modules/location.js'
+import{zeroState} from './modules/zeroState.js'
 
 
 let app = {
@@ -10,6 +11,7 @@ let app = {
     mainBook.init()
     result.init()
     bookLocation.init()
+    zeroState.init()
   }
 }
 
